520e2d7fb3fd2712e55875fc89ba216648161ab6
Daniel <cawasdn@gmail.com>
2023-09-26 11:39:04 +0800

    Added Context p2

---
+++ frontend/src/hooks/useWorkoutsContext.js
@@ -0,0 +1,14 @@
+import { WorkoutsContext, useReducer } from '../context/WorkoutContext'
+import { useContext } from 'react'
+
+export const useWorkoutsContext = () => {
+  const context = useContext(WorkoutsContext)
+
+  if (!context) {
+    throw Error(
+      'useWorkoutsContext must be used inside a WorkoutsContextProvider'
+    )
+  }
+
+  return context
+}
